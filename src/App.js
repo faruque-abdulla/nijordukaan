a820128e9d39a198df85dd021b461cacb7213e32
Faruque Abdulla <breakneck6303@gmail.com>
2025-03-18 23:07:10 +0530

    Fixed routing issue with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// react 
+// react
 import React from "react";
 // css
 import "./App.css";
-// browserrouter 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// hashrouter
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 // Components
-import Header from './Component/Header';
+import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 // pages
 import Home from "./pages/Home";
@@ -33,13 +33,14 @@ import MyAccountAddress from "./pages/Accounts/MyAccountAddress";
 import MyAccountForgetPassword from "./pages/Accounts/MyAccountForgetPassword";
 import MyAccountSignIn from "./pages/Accounts/MyAccountSignIn";
 import MyAccountSignUp from "./pages/Accounts/MyAccountSignUp";
+
 const App = () => {
   return (
     <div>
       <Router>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/Grocery-react/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           {/* Shop pages */}
           <Route path="/Shop" element={<Shop />} />
           <Route path="/ShopGridCol3" element={<ShopGridCol3 />} />
@@ -53,10 +54,19 @@ const App = () => {
           {/* Accounts pages */}
           <Route path="/MyAccountOrder" element={<MyAccountOrder />} />
           <Route path="/MyAccountSetting" element={<MyAccountSetting />} />
-          <Route path="/MyAcconutNotification" element={<MyAcconutNotification />} />
-          <Route path="/MyAcconutPaymentMethod" element={<MyAcconutPaymentMethod />} />
+          <Route
+            path="/MyAcconutNotification"
+            element={<MyAcconutNotification />}
+          />
+          <Route
+            path="/MyAcconutPaymentMethod"
+            element={<MyAcconutPaymentMethod />}
+          />
           <Route path="/MyAccountAddress" element={<MyAccountAddress />} />
-          <Route path="/MyAccountForgetPassword" element={<MyAccountForgetPassword />} />
+          <Route
+            path="/MyAccountForgetPassword"
+            element={<MyAccountForgetPassword />}
+          />
           <Route path="/MyAccountSignIn" element={<MyAccountSignIn />} />
           <Route path="/MyAccountSignUp" element={<MyAccountSignUp />} />
           {/* About pages */}
@@ -65,7 +75,7 @@ const App = () => {
           <Route path="/Contact" element={<Contact />} />
           <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
